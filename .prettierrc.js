bb161039567bbdae6bb7dbe3c47b161f2a2cf908
mouse_484 <escmouse484@gmail.com>
2019-04-08 00:05:25 +0000

    色々セッティ

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+    tabWidth: 4,
+    useTabs:false,
+    semi:true,
+    singleQuote:false,
+    trailingComma:"es5",
+    bracketSpacing:true,
+    arrowParens: "avoid",
+};
