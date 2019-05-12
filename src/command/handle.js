22d7963b07c826364ccd1068942f4789e3e4103e
mouse484 <escmouse484@gmail.com>
2019-05-12 19:54:34 +0900

    prefix コマンド

--- src/command/handle.js
+++ src/command/handle.js
@@ -1,44 +1,24 @@
 class command_handle {
     constructor(client, message) {
-        const { prefix } = client.options;
-
-        if (message.author.bot) return;
-
+        this.message = message;
+        this.prefix = client.options.prefix;
         this.command = {};
 
-        if (message.content.startsWith(prefix)) {
-            this.command.name = message.content
-                .slice(prefix.length)
-                .trim()
-                .split(/ +/g)[0];
-            this.command.value = message.content
-                .slice(prefix.length + this.command.name.length)
-                .trim();
+        if (this.message.author.bot) return this.error("bot");
 
-            console.log(this.command.name, this.command.value);
-        } else if (message.channel.type === "dm") {
-            this.command.name = message.content.split(" ")[0];
-            this.command.value = message.content
-                .slice(this.command.name.length)
-                .trim();
-        } else return;
+        if (this.message.content.startsWith(this.prefix)) this.prefixcmd();
+    }
 
-        message.command = this.command;
-        const command = client.commands[message.command.name];
+    error(reason) {
+        this.status = { status: "error", reason };
+        return this.status;
+    }
 
-        if (!command) return;
-
-        if (command.info.args) {
-            const args = {};
-            Object.keys(command.info.args).forEach(ar => {
-                console.log(ar);
-                args[ar] = client.args[command.info.args[ar]].parse(
-                    message.command.value
-                );
-            });
-            return command.run(message, args);
-        }
-        return command.run(message);
+    prefixcmd() {
+        this.command.name = this.message.content
+            .slice(this.prefix.length)
+            .split(" ")[0];
+        return this.command;
     }
 }
 
