5547c5975b92e101763d485529340be7bfde5efc
mouse_484 <38714187+mouse484@users.noreply.github.com>
2019-06-29 10:46:59 +0900

    one Args (#11)

---
+++ src/command/run.js
@@ -0,0 +1,26 @@
+const error = require("../errors/command.js");
+
+class command_run {
+    constructor(client, message, command_data) {
+        this.message = message;
+        this.client = client;
+        const command = this.client.commands[command_data.name];
+
+        if (!command) return new error("no command");
+
+        if (command.info.args) {
+            this.args = {};
+            Object.keys(command.info.args).forEach(args_name => {
+                const args_type = command.info.args[args_name];
+                const argument = client.args[args_type];
+                if (argument) {
+                    this.args[args_name] = argument.parse(command_data.value);
+                }
+            });
+            return command.run(this.message, this.args);
+        }
+        return command.run(this.message, "");
+    }
+}
+
+module.exports = command_run;
