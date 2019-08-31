e88defd1db5c9d2e11090c133521b045eb0e4697
mouse484 <escmouse484@gmail.com>
2019-08-31 10:53:39 +0900

    reset

--- src/errors/command.js
+++
@@ -1,7 +0,0 @@
-class command_error {
-    constructor(reason) {
-        this.status = { status: "error", reason };
-        return this.status;
-    }
-}
-module.exports = command_error;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable global-require*/
-
-module.exports = {
-    client: require("./client"),
-    command: require("./command/create"),
-};
