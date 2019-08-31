6433d3819904fe6c365a9eed049031277ad52c87
mouse_484 <38714187+mouse484@users.noreply.github.com>
2019-08-31 17:29:43 +0900

    message prefix(Command）check (#22)

--- src/lib/dispatcher.js
+++ src/lib/dispatcher.js
@@ -1,12 +1,25 @@
-class dispatcher{
+class dispatcher {
     constructor(client) {
-        this = client;
+        this.client = client;
     }
+
     messageHandle(message) {
         if (!this.messageCheck(message)) return;
+        if (!this.commandCheck(message)) return;
+
+        this.client.logger.trace(`[Command] : ${message.content}`);
+        message.channel.send(`[Command] : ${message.content}`);
     }
+
     messageCheck(message) {
         if (message.author.bot) return false;
         return true;
     }
+
+    commandCheck(message) {
+        if (message.content.startsWith(this.client.options.prefix)) return true;
+        return false;
+    }
 }
+
+module.exports = dispatcher;
