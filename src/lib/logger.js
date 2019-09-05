6ec04afe46ac7cf5ae88fac6e4e835d9179226dd
mouse_484 <escmouse484@gmail.com>
2019-09-05 09:01:33 +0000

    loggers

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -6,6 +6,15 @@ class logger {
     info(args) {
         return log(`${chalk.blue("[info]")} ${args}`);
     }
+    command(args) {
+        return log(`${chalk.green("[command]")} ${args}`);
+    }
+    error(args) {
+        return log(`${chalk.red("[error]")} ${args}`);
+    }
+    warn(args) {
+        return log(`${chalk.yellow("[warn]")} ${args}`);
+    }
 }
 
 module.exports = logger;
