fec825bbb217717aaf84fac2da8613f9cf7e6e08
mouse_484 <escmouse484@gmail.com>
2019-09-06 02:37:47 +0000

    ✨ : Add initial code command base

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,3 +1,14 @@
-class Command {}
+const logger = new (require("../lib/logger.js"))();
+class Command {
+    constructor(client, info) {
+        this.info_check(client, info);
+        this.client = client;
+        this.info = info;
+    }
+
+    static info_check(client, info) {
+        if (!client) logger.error("A client must be specified.");
+    }
+}
 
 module.exports = Command;
