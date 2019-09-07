c8dd8ec58059c53ef6a0ea4cbd185be28a27d193
mouse_484 <escmouse484@gmail.com>
2019-09-07 07:07:21 +0000

    ✨ : Add test command name

--- test/bot/commands/app/test.js
+++ test/bot/commands/app/test.js
@@ -3,7 +3,7 @@ const { Command } = require("../../../../src");
 module.exports = class extends Command {
     constructor(client, info) {
         super(client, {
-            
+            name: "test",
         });
     }
 };
