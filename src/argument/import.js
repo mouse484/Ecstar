1518dbdbabcf32671c66e1cb7fb4f37972556910
mouse_484 <escmouse484@gmail.com>
2019-09-13 03:19:57 +0000

    ✨ : Add new function argument import

---
+++ src/argument/import.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const ignore = ["index.js", "import.js"];
+
+class ArgumentImport {
+    constructor(client) {
+        const args = {};
+
+        fs.readdir(__dirname, (err, dir) => {
+            if (err) client.logger.error(err);
+            dir.filter(file => !ignore.includes(file)).forEach(file => {
+                /* eslint-disable global-require */
+                const argument_file = require(`./${file}`);
+                const argument = new argument_file(client);
+
+                args[argument.type] = argument;
+            });
+        });
+
+        client.arguments = args;
+        client.logger.info("Success import arguments");
+    }
+}
+
+module.exports = ArgumentImport;
