62ebd2f9f9144bf9c1a303698d85ff65f6b6b6d7
mouse_484 <escmouse484@gmail.com>
2019-09-26 08:29:58 +0000

    🔥 : Remove files Listener

--- src/event/listener.js
+++
@@ -1,9 +0,0 @@
-class EventListener {
-    constructor(client, events, callback) {
-        this.client = client;
-        if (!client.on) return;
-        events.forEach(event => {
-            emitter.on(event.name, callback)
-        });
-    }
-}
