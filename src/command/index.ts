bd13cc6dad6759a38fb16f5170fac9ff1d31381f
mouse <escmouse484@gmail.com>
2019-12-18 16:21:20 +0900

    :label: command

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,9 +1,16 @@
 import print from '../lib/print';
+import { EcstarClient } from '../client';
+
+interface infoType {
+  name: string;
+  aliases?: string[];
+  description?: string;
+}
 
 export class CommandBase {
-  client: any;
-  info: string;
-  constructor(client, info) {
+  client: EcstarClient;
+  info: infoType;
+  constructor(client: EcstarClient, info: infoType) {
     if (!client) print.error('A client must be specified.');
     if (!info.name) print.error('command name must be specified.');
     this.client = client;
