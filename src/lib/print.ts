46a26b7412de251117d9e65daf179570a17f426c
mouse_484 <escmouse484@gmail.com>
2019-12-26 04:00:34 +0000

    Visual logs

--- src/lib/print.ts
+++ src/lib/print.ts
@@ -2,30 +2,29 @@ import chalk from 'chalk';
 import figures from 'figures';
 const { exit } = process;
 
-const log = (args: string) => {
-  console.log(`${new Date().toLocaleTimeString()}  ${args}`);
+const log = (type: string, ...args: string[]) => {
+  console.log(`${new Date().toLocaleTimeString()} | ${type}\t${args}`);
 };
 
 export = {
   info(args: string) {
-    return log(chalk`{blue ${figures.info} info} ${args}`);
+    return log(chalk.blue(figures.info, 'info'), args);
   },
   import(type: 'command', name: string, path: string) {
     return log(
-      chalk`{magenta ${figures.circleCircle} import} ${name} {gray ${path}}`
+      chalk.magenta(figures.circleCircle, 'import'),
+      name,
+      chalk.gray(path)
     );
   },
   command(args: string) {
-    return log(`${chalk.green('[command]')} ${args} `);
+    return log(chalk.green(figures.play, 'command'), args);
   },
   error(args: string) {
-    log(`${chalk.red('[error]')} ${args} `);
+    log(chalk.red(figures.cross, 'error'), args);
     return exit();
   },
   warn(args: string) {
-    return log(`${chalk.yellow('[warn]')} ${args} `);
-  },
-  dev(args: string) {
-    return log(`${chalk.black('[dev]')} ${args} `);
+    return log(chalk.yellow(figures.warning, 'warn'), args);
   },
 };
