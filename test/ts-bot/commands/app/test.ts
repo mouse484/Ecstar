617f8a3e194456975fa6a8cc845ce26b339ee421
mouse_484 <escmouse484@gmail.com>
2020-01-25 06:48:20 +0000

    add command description

--- test/ts-bot/commands/app/test.ts
+++ test/ts-bot/commands/app/test.ts
@@ -2,7 +2,7 @@ import { Command, Client, Message } from '../../../../src';
 
 export = class extends Command {
   constructor(client: Client) {
-    super(client, { name: 'test' });
+    super(client, { name: 'test', description: 'test command' });
   }
 
   run(message: Message) {
