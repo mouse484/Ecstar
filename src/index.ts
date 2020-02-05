d22fc5e9d4851cb5acf440687bd9fc9d368186d3
mouse_484 <escmouse484@gmail.com>
2020-02-05 03:59:56 +0000

    Change Export style

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import libDirectory from './lib/directory';
-import libPrint from './lib/print';
-
 export { EcstarClient as Client } from './client';
 export { CommandBase as Command } from './command';
 export { EventBase as Event } from './event';
 export { LangBase as Lang } from './lang';
-export const print = libPrint;
-export const directory = libDirectory;
+export { default as print } from './lib/print';
+export { default as directory } from './lib/directory';
 export * from 'discord.js';
