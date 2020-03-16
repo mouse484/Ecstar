73aeb14970b5c4f6caed863c064d71ef440bfc59
mouse_484 <escmouse484@gmail.com>
2020-03-16 02:23:44 +0000

    from client.ts to Client.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export {
   Snowflake,
   Message,
 } from 'discord.js';
-export { EcstarClient as Client } from './client';
+export { EcstarClient as Client } from './Client';
 export * from './lib';
 export { CommandBase as Command } from './command/Base';
 export { EventBase as Event } from './event/Base';
