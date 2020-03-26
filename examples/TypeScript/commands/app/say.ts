900442a728e69c4be69c331a57d89909eb492382
mouse_484 <escmouse484@gmail.com>
2020-03-26 09:05:54 +0000

    Modification of sample command arguments

--- examples/TypeScript/commands/app/say.ts
+++ examples/TypeScript/commands/app/say.ts
@@ -2,10 +2,14 @@ import { Command, Client, Message } from '../../../../src';
 
 export = class extends Command {
   constructor(client: Client) {
-    super(client, { name: 'say', args: true, description: 'Reply message' });
+    super(client, {
+      name: 'say',
+      args: { text: 'string' },
+      description: 'Reply message',
+    });
   }
 
-  run(message: Message, { all }: { all: string }) {
-    return message.channel.send(all);
+  run(message: Message, { text }: { text: string }) {
+    return message.channel.send(text);
   }
 };
