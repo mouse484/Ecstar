e49412fd1444364c348d78f88fcf030a1fd55ddf
mouse_484 <escmouse484@gmail.com>
2020-03-28 06:23:59 +0000

    Channel　SAMPLE

---
+++ examples/TypeScript/commands/args/channel.ts
@@ -0,0 +1,31 @@
+import {
+  Command,
+  Client,
+  Message,
+  isTextChannel,
+  isVoiceChannel,
+} from '../../../../src';
+import {
+  Channel,
+  TextChannel,
+  VoiceChannel,
+  DMChannel,
+  MessageEmbed,
+} from 'discord.js';
+
+export = class extends Command {
+  constructor(client: Client) {
+    super(client, {
+      name: 'channel',
+      args: { channel: 'channel' },
+    });
+  }
+
+  run(message: Message, { channel }: { channel: Channel }) {
+    const embed = new MessageEmbed();
+    if (isTextChannel(channel) || isVoiceChannel(channel)) {
+      embed.addField('name', channel.name).addField('type', channel.type);
+    }
+    return message.channel.send(embed);
+  }
+};
