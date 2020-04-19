acfc8dfe27d11b49b8b861af7198c0918fba1b0d
mouse_484 <escmouse484@gmail.com>
2020-04-19 07:40:43 +0000

    Passing DiscordMessage to args

--- src/args/default/channel.ts
+++ src/args/default/channel.ts
@@ -1,12 +1,12 @@
 import { Client, Args } from 'ecstar';
-import { Channel } from 'discord.js';
+import { Message, Channel } from 'discord.js';
 
 export = class extends Args {
   constructor(client: Client) {
     super(client, 'channel');
   }
-  run(message: string): Channel {
-    const channel = this.client.channels.cache.get(message);
+  run(message: Message, text: string): Channel {
+    const channel = this.client.channels.cache.get(text);
     if (!channel) throw this.client.lang.INVALID_ARGUMENT(this.options.name);
     return channel;
   }
--- src/args/default/number.ts
+++ src/args/default/number.ts
@@ -1,11 +1,12 @@
 import { Client, Args } from 'ecstar';
+import { Message } from 'discord.js';
 
 export = class extends Args {
   constructor(client: Client) {
     super(client, 'number');
   }
-  run(message: string): number {
-    const parsed = parseInt(message);
+  run(message: Message, text: string): number {
+    const parsed = parseInt(text);
 
     if (Number.isNaN(parsed)) {
       throw this.client.lang.INVALID_ARGUMENT(this.options.name);
