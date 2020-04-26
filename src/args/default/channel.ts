793ad17024ca7b8c6f5437ad10c270b46967c510
mouse_484 <escmouse484@gmail.com>
2020-04-26 04:45:16 +0000

    Allow search by channel name

--- src/args/default/channel.ts
+++ src/args/default/channel.ts
@@ -1,4 +1,4 @@
-import { Client, Args } from 'ecstar';
+import { Client, Args, isTextChannel, isVoiceChannel } from 'ecstar';
 import { Message, Channel } from 'discord.js';
 
 export = class extends Args {
@@ -6,7 +6,11 @@ export = class extends Args {
     super(client, 'channel');
   }
   run(message: Message, text: string): Channel {
-    const channel = this.client.channels.cache.get(text);
+    const channel = this.client.channels.cache.find((value) =>
+      isTextChannel(value) || isVoiceChannel(value)
+        ? value.name === text
+        : value.id === text
+    );
     if (!channel) throw this.client.lang.INVALID_ARGUMENT(this.options.name);
     return channel;
   }
