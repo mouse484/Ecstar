e2345757b697f75f494e8c0c8f67970cfb70be67
mouse_484 <escmouse484@gmail.com>
2020-04-27 02:29:35 +0000

    add args/role fix #299

---
+++ src/args/default/role.ts
@@ -0,0 +1,15 @@
+import { Client, Args, equalsIgnoreCase } from 'ecstar';
+import { Message, Role } from 'discord.js';
+
+export = class extends Args {
+  constructor(client: Client) {
+    super(client, 'role');
+  }
+  run(message: Message, text: string): Role {
+    const role = message.guild?.roles.cache.find(
+      (value) => value.id === text || equalsIgnoreCase(value.name, text)
+    );
+    if (!role) throw this.client.lang.INVALID_ARGUMENT(this.options.name);
+    return role;
+  }
+};
