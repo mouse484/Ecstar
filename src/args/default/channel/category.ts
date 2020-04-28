f0920d2c7390d305c930f1f2eadfebd51fc14561
mouse_484 <escmouse484@gmail.com>
2020-04-28 06:17:16 +0000

    add args category

---
+++ src/args/default/channel/category.ts
@@ -0,0 +1,16 @@
+import { Client, Args, isCategoryChannel } from 'ecstar';
+import { Message, Channel } from 'discord.js';
+
+export = class extends Args {
+  constructor(client: Client) {
+    super(client, 'category');
+  }
+  run(message: Message, text: string): Channel {
+    const channel = this.client.channels.cache.find(
+      (value) =>
+        isCategoryChannel(value) && (value.name === text || value.id === text)
+    );
+    if (!channel) throw this.client.lang.INVALID_ARGUMENT(this.options.name);
+    return channel;
+  }
+};
