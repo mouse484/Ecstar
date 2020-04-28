9fd9c715f0c1dd94a94227150d86ba150eae1c28
mouse_484 <escmouse484@gmail.com>
2020-04-28 06:10:56 +0000

    add args dm-channel

---
+++ src/args/default/channel/dm-channel.ts
@@ -0,0 +1,15 @@
+import { Client, Args, isDMChannel } from 'ecstar';
+import { Message, Channel } from 'discord.js';
+
+export = class extends Args {
+  constructor(client: Client) {
+    super(client, 'voice-channel');
+  }
+  run(message: Message, text: string): Channel {
+    const channel = this.client.channels.cache.find(
+      (value) => isDMChannel(value) && value.id === text
+    );
+    if (!channel) throw this.client.lang.INVALID_ARGUMENT(this.options.name);
+    return channel;
+  }
+};
