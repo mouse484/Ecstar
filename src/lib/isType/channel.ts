1dbc819360f06cafcfec7ad3823f317e22055b12
mouse_484 <escmouse484@gmail.com>
2020-04-28 06:15:55 +0000

    add is channeltype

--- src/lib/isType/channel.ts
+++ src/lib/isType/channel.ts
@@ -1,6 +1,24 @@
-import { Channel, TextChannel, VoiceChannel } from 'discord.js';
+import {
+  Channel,
+  DMChannel,
+  TextChannel,
+  VoiceChannel,
+  CategoryChannel,
+  NewsChannel,
+  StoreChannel,
+} from 'discord.js';
 
+interface NamedChannel extends Channel {
+  name: string;
+}
 
+export const isNamedChannel = (channel: Channel): channel is NamedChannel => {
+  return ['text', 'voice', 'category', 'news', 'store'].includes(channel.type);
+};
+
+export const isDMChannel = (channel: Channel): channel is DMChannel => {
+  return channel.type === 'dm';
+};
 
 export const isTextChannel = (channel: Channel): channel is TextChannel => {
   return channel.type === 'text';
@@ -8,4 +26,18 @@ export const isTextChannel = (channel: Channel): channel is TextChannel => {
 
 export const isVoiceChannel = (channel: Channel): channel is VoiceChannel => {
   return channel.type === 'voice';
+};
+
+export const isCategoryChannel = (
+  channel: Channel
+): channel is CategoryChannel => {
+  return channel.type === 'category';
+};
+
+export const isNewsChannel = (channel: Channel): channel is NewsChannel => {
+  return channel.type === 'news';
+};
+
+export const isStoreChannel = (channel: Channel): channel is StoreChannel => {
+  return channel.type === 'store';
 };
