a169be73aa32f4d194fd69c10dc62702024c9e9d
mouse_484 <escmouse484@gmail.com>
2020-04-28 05:56:38 +0000

    add args voice-channel

---
+++ src/args/default/channel/voice-channel.ts
@@ -0,0 +1,16 @@
+import { Client, Args, isVoiceChannel } from 'ecstar';
+import { Message, Channel } from 'discord.js';
+
+export = class extends Args {
+  constructor(client: Client) {
+    super(client, 'voice-channel');
+  }
+  run(message: Message, text: string): Channel {
+    const channel = this.client.channels.cache.find(
+      (value) =>
+        isVoiceChannel(value) && (value.name === text || value.id === text)
+    );
+    if (!channel) throw this.client.lang.INVALID_ARGUMENT(this.options.name);
+    return channel;
+  }
+};
