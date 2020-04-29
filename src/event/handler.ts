26b9c6961821e98d4b0819cb7f510f8f370a5f9a
mouse_484 <escmouse484@gmail.com>
2020-04-29 04:22:01 +0000

    fix linter errors

--- src/event/handler.ts
+++ src/event/handler.ts
@@ -1,5 +1,5 @@
 import { Client, print, Event } from 'ecstar';
-import { ClientEvents, Message } from 'discord.js';
+import { Message } from 'discord.js';
 
 import MessageEvent from './default/message';
 
@@ -12,10 +12,11 @@ export const eventHandler = (
     case 'ready':
       print.info(`${client.lang.BOT_READY} ${client.user?.tag}`);
       break;
-    case 'message':
-      const message = callback[0];
+    case 'message': {
+      const [message] = callback;
       if (message instanceof Message) new MessageEvent(client).run(message);
       break;
+    }
     default: {
       const event: Event | undefined = client.events.get(name);
       if (event) {
