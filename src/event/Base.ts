2730fd1e3cf3f71108c1e9f39fa4d9147b135a04
mouse_484 <escmouse484@gmail.com>
2020-05-30 05:40:46 +0000

    Event can reload

--- src/event/Base.ts
+++ src/event/Base.ts
@@ -2,6 +2,7 @@ import { Client, EcstarFile } from 'ecstar';
 import { ClientEvents } from 'discord.js';
 
 export abstract class EventBase extends EcstarFile {
+  bindedRun = this.run.bind(this);
   constructor(public client: Client, public name: keyof ClientEvents) {
     super(client, name);
   }
--- src/event/Store.ts
+++ src/event/Store.ts
@@ -6,14 +6,26 @@ export class EventStore extends Store<Event> {
   }
   import(path: string): void {
     const file: Event = super.getFile(path);
-    print.store(this.type, 'import', file.name, path);
-    this.set(path, file);
 
-    this.client.on(file.name, (...args) => {
-      file.run(...args);
-    });
+    print.store(this.type, 'import', file.name, path);
+    super.set(path, file);
+
+    this.client.on(file.name, file.bindedRun);
   }
   update(path: string): void {
-    print.warn(`Event file can't auto update, please restart. ${path}`);
+    let file = super.get(path);
+
+    if (!file) return;
+    this.client.off(file.name, file.bindedRun);
+    super.delete(file.name);
+
+    delete require.cache[path];
+    file = super.getFile(path);
+
+    if (!file) return;
+    this.client.on(file.name, file.bindedRun);
+    super.set(file.name, file);
+
+    print.store(this.type, 'update', file.name, path);
   }
 }
